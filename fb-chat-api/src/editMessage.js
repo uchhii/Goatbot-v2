17654bfa6606b18a585fddbab65ec3778c978e18
ＮＡＺＲＵＬ <161818252+nazrul4x@users.noreply.github.com>
2025-11-24 01:09:27 +0000

    Ok

--- fb-chat-api/src/editMessage.js
+++ fb-chat-api/src/editMessage.js
@@ -1,66 +1,76 @@
 "use_strict";
-/**
- * @author RFS-ADRENO
- * @rewrittenBy Isai Ivanov
- */
-const generateOfflineThreadingId = require('../utils');
 
-function canBeCalled(func) {
-	try {
-		Reflect.apply(func, null, []);
-		return true;
-	} catch (error) {
-		return false;
-	}
+// 02-14-2025
+
+/*
+Feature: Add callback function for editMessage #6 Merged
+NethWs3Dev merged 1 commit into NethWs3Dev:main from VangBanLaNhat:Pull_requests
+*/
+
+// I improved some code.
+const utils = require("../utils");
+module.exports = (defaultFuncs, api, ctx) => {
+  return (text, messageID, callback) => {
+    let reqID = ctx.wsReqNumber + 1;
+    var resolveFunc = () => {};
+    var rejectFunc = () => {};
+    var returnPromise = new Promise((resolve, reject) => {
+      resolveFunc = resolve;
+      rejectFunc = reject;
+    });
+    if (!callback) {
+      callback = (err, data) => {
+        if (err) {
+          return rejectFunc(err);
+        }
+        resolveFunc(data);
+      };
+    }
+    const content = {
+      app_id: '2220391788200892',
+      payload: JSON.stringify({
+        data_trace_id: null,
+        epoch_id: parseInt(utils.generateOfflineThreadingID()),
+        tasks: [{
+          failure_count: null,
+          label: '742',
+          payload: JSON.stringify({
+            message_id: messageID,
+            text: text,
+          }),
+          queue_name: 'edit_message',
+          task_id: ++ctx.wsTaskNumber,
+        }],
+        version_id: '6903494529735864',
+      }),
+      request_id: ++ctx.wsReqNumber,
+      type: 3
+    }
+    ctx.mqttClient.publish('/ls_req', JSON.stringify(content), {
+      qos: 1,
+      retain: false
+    });
+    const handleRes = (topic, message) => {
+      if (topic === "/ls_resp") {
+        let jsonMsg = JSON.parse(message.toString());
+        jsonMsg.payload = JSON.parse(jsonMsg.payload);
+        if (jsonMsg.request_id != reqID) return;
+        ctx.mqttClient.removeListener('message', handleRes);
+        let msgID = jsonMsg.payload.step[1][2][2][1][2];
+        let msgReplace = jsonMsg.payload.step[1][2][2][1][4];
+        const bodies = {
+          body: msgReplace,
+          messageID: msgID
+        };
+        if (msgReplace != text) {
+          return callback({
+            error: "The message is too old or not from you!"
+          }, bodies);
+        }
+        return callback(undefined, bodies);
+      }
+    }
+    ctx.mqttClient.on('message', handleRes);
+    return returnPromise;
+  };
 }
-
-/**
- * A function for editing bot's messages.
- * @param {string} text - The text with which the bot will edit its messages.
- * @param {string} messageID - The message ID of the message the bot will edit.
- * @param {Object} callback - Callback for the function.
- */
-
-module.exports = function (defaultFuncs, api, ctx) {
-	return function editMessage(text, messageID, callback) {
-		if (!ctx.mqttClient) {
-			throw new Error('Not connected to MQTT');
-		}
-
-		ctx.wsReqNumber += 1;
-		ctx.wsTaskNumber += 1;
-
-		const queryPayload = {
-			message_id: messageID,
-			text: text
-		};
-
-		const query = {
-			failure_count: null,
-			label: '742',
-			payload: JSON.stringify(queryPayload),
-			queue_name: 'edit_message',
-			task_id: ctx.wsTaskNumber
-		};
-
-		const context = {
-			app_id: '2220391788200892',
-			payload: {
-				data_trace_id: null,
-				epoch_id: parseInt(generateOfflineThreadingId),
-				tasks: [query],
-				version_id: '6903494529735864'
-			},
-			request_id: ctx.wsReqNumber,
-			type: 3
-		};
-
-		context.payload = JSON.stringify(context.payload);
-
-		// if (canBeCalled(callback)) {
-		// 	ctx.reqCallbacks[ctx.wsReqNumber] = callback;
-		// }
-
-		ctx.mqttClient.publish('/ls_req', JSON.stringify(context), { qos: 1, retain: false });
-	};
-};
